13decf45f8c1f52bb670c319dab1f14e3819a25d
JDCodeWork <jdorozco792006@gmail.com>
2025-05-30 19:22:37 -0500

    feat: update project configuration and enhance HomePage with error handling and project loading

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -1,13 +1,41 @@
 import { FeaturedProjects } from "@features/home/components/FeaturedProjects";
 import { HeroSection } from "@features/home/components/HeroSection";
+import { getPaginatedProjects } from "@features/projects";
+import { useEffect, useState } from "react";
+import {
+	Outlet,
+	data,
+	isRouteErrorResponse,
+	useRouteError,
+} from "react-router";
+import { toast } from "sonner";
+import type { Route } from "./+types/home";
 
-const HomePage = () => {
+export async function loader(args: Route.LoaderArgs) {
+	try {
+		throw new Error("This is a test error to demonstrate error handling in the loader function.");
+		const { projects } = await getPaginatedProjects({
+			filter: "featured",
+			page: 1,
+			limit: 6,
+		});
+
+		return { projects }
+	} catch (error) {
+		return { error: "Failed to load featured projects" }
+	}
+}
+
+export default function HomePage({ loaderData }: Route.ComponentProps) {
+	if( loaderData.error ) {
+		toast.error(loaderData.error);
+	}
+
 	return (
 		<>
 			<HeroSection />
-			<FeaturedProjects />
+			<FeaturedProjects projects={loaderData.projects || []} />
 		</>
 	);
-};
+}
 
-export default HomePage;
